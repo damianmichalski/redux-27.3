056806287800963f8e57a0d389d25434630280b6
DamianM <damian.michalski@kramp.com>
2019-02-11 10:56:40 +0100

    Redux - akcje

---
+++ actions.js
@@ -0,0 +1,47 @@
+import uuid from 'uuid';
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    id: uuid.v4(),
+    text
+  }
+}
+
+
+function removeComment(id) {
+  return {
+    type: REMOVE_COMMENT,
+    id
+  }
+}
+
+function editComment(id, text) {
+  return {
+    type: EDIT_COMMENT,
+    id,
+    text
+  }
+}
+
+function upComment(id, amount) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id,
+    amount
+  }
+}
+
+function downComment(id, amount) {
+  return {
+    type: THUMB_DOWN_COMMENT,
+    id,
+    amount
+  }
+}
